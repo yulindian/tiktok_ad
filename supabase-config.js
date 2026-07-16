623ffeb385bb8f0655757f4b9f06363029832ee0
yulindian <tianmu_0805@qq.com>
2026-07-16 15:45:05 +0800

    Update Supabase URL in configuration

--- supabase-config.js
+++ supabase-config.js
@@ -1,4 +1,4 @@
 window.SUPABASE_CONFIG = {
-  url: "https://gtbmnmsvbgruhptzacad.supabase.co/rest/v1/",
+  url: "https://gtbmnmsvbgruhptzacad.supabase.co/",
   anonKey: "sb_publishable_rxbVRoV5RfX1IdV5d4qh-w_ITegwtuA",
 };
